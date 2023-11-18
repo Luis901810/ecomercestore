edca634dcf126818be9425f09b4754b0140ac64b
unknown <luis901810@gmail.com>
2023-11-18 18:45:36 -0500

    subo posgret

--- src/db.js
+++ src/db.js
@@ -2,15 +2,27 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+//const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
-  {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+  dialect: "postgres",
+  logging: false,
+  ssl: process.env.DATABASE_URL ? true : false,
+  dialectOptions: {
+    ssl: process.env.DATABASE_URL ? { require: true, rejectUnauthorized: false } : false,
+  },
+});
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Conexión a PostgreSQL exitosa");
+  } catch (error) {
+    console.error("Error al conectar con PostgreSQL:", error);
   }
-);
+})();
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
@@ -61,4 +73,5 @@ Orders.belongsToMany(Videogames, { through: 'OrderProducts', foreignKey: 'orderI
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize, // para importart la conexión { conn } = require('./db.js');
+ 
 };
