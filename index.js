0c369b622d4463687315b624f0650d6113a0f085
unknown <luis901810@gmail.com>
2023-11-18 17:16:13 -0500

    subo actualizacion del backen en index

--- index.js
+++ index.js
@@ -3,17 +3,14 @@ const { conn } = require("./src/db");
 
 const port = process.env.PORT || 3002;
 
-// Syncing all the models at once:
+//Syncing all the models at once:
 conn.sync({ alter: true }).then(() => {
   server.get("/", (req, res) => {
     res.send('Bienvenidos al Backend de eCommerce');
   });
 
   server.listen(port, () => {
-    console.log(`Servidor escuchando en localhost:${port}`);
+    console.log(`puerto escuchando en localhost:${port}`); // eslint-disable-line no-console
   });
+
 });
-
-
-
-
