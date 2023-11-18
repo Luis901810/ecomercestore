741c91fa14a99b65829f43e849dd90be301019a0
unknown <luis901810@gmail.com>
2023-11-18 17:23:47 -0500

    subo actualizacion del backen en index2

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ const port = process.env.PORT || 3002;
 
 //Syncing all the models at once:
 conn.sync({ alter: true }).then(() => {
-  server.get("/", (req, res) => {
-    res.send('Bienvenidos al Backend de eCommerce');
-  });
-
   server.listen(port, () => {
     console.log(`puerto escuchando en localhost:${port}`); // eslint-disable-line no-console
   });
