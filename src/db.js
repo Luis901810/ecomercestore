6b2f5face28a1737d29cbcdde1d65e3d6fcc0910
unknown <luis901810@gmail.com>
2023-11-18 21:27:55 -0500

    deplooy

--- src/db.js
+++ src/db.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
+const pg = require('pg');
 const fs = require("fs");
 const path = require("path");
 //const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
