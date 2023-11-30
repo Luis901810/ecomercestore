6cbe196e2583e032edf4e1f2e3b6d8ece1bea177
unknown <luis901810@gmail.com>
2023-11-29 19:22:11 -0500

    actualizacion

--- src/routes/mercadoPago.js
+++ src/routes/mercadoPago.js
@@ -3,7 +3,7 @@ const mercadopago = require("mercadopago");
 
 //!Credenciales del .env
 const ACCESS_TOKEN= "TEST-7120791930180798-112715-b8bed09589c90708ab8b212544f746b9-1555519585"
-const LOCALHOST =['http://localhost:3000', 'https://e-comercefrom.vercel.app'];
+const LOCALHOST =['http://localhost:3000', 'https://ecomercefromdeploy.vercel.app/'];
 
 const mercadoPago = Router();
 
